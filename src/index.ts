4a62dbaaef6aa3f6412d9df13c074010568f02ab
calezich <nicolas.calezich@accenture.com>
2021-11-21 13:49:32 -0300

    Feat: added Cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import mongoose from "mongoose"
+import cors from "cors";
+
 
 const app = express();
 const port = 8080; // default port to listen
+
+app.use(cors({
+    origin: '*'
+}));
 
 import reposRoute from './routes/repos'
 
 app.use(express.json());
 app.use('/repos', reposRoute);
+
 
 // start the Express server
 app.listen( port, async () => {
